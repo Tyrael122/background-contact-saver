c3546c97fe3abd1469c4c3e2ca43c756ce3dcb0b
Tyrael122 <kanbme@protonmail.com>
2023-07-28 18:05:27 -0300

    Bug fixes

---
+++ android/app/src/main/java/br/makesoftware/contacts_manager/MainActivity.java
@@ -0,0 +1,52 @@
+package br.makesoftware.contacts_manager;
+
+import androidx.annotation.NonNull;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
+
+import java.util.concurrent.TimeUnit;
+
+import br.makesoftware.contacts_manager.constants.LogConstants;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+
+public class MainActivity extends FlutterActivity {
+    private static final String CHANNEL = "br.makesoftware.contacts_manager/channel";
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler((call, result) -> {
+            switch (call.method) {
+                case "startService":
+                    int repeatInterval = call.argument("requestInterval");
+//                    PeriodicWorkRequest contactsWorkRequest = new PeriodicWorkRequest.Builder(ContactWorker.class, repeatInterval, TimeUnit.MINUTES).build();
+                    OneTimeWorkRequest contactsWorkRequest = new OneTimeWorkRequest.Builder(ContactWorker.class)
+                            .setInitialDelay(10, TimeUnit.SECONDS)
+                            .build();
+
+                    WorkManager.getInstance(getApplicationContext()).enqueue(contactsWorkRequest);
+
+                    result.success(true);
+                    break;
+                case "stopService":
+                    boolean hasStoppedSucessfully = ContactWorker.stopAllServices(getApplicationContext());
+                    result.success(hasStoppedSucessfully);
+                    break;
+                case "logError":
+                    new FileLogger(getApplicationContext().getFilesDir(), LogConstants.STATUS_LOGGER_NAME).logError(call.argument("message"));
+                    break;
+                case "logInfo":
+                    new FileLogger(getApplicationContext().getFilesDir(), LogConstants.STATUS_LOGGER_NAME).logInfo(call.argument("message"));
+                    break;
+                default:
+                    result.notImplemented();
+                    break;
+            }
+        });
+    }
+}
+
+
